c7850da49342be5c1ffc37c3fc583c3982e30a38
Alexey Sidorovitch <sidorovitch@gmail.com>
2018-04-11 20:46:15 +0300

    search controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,37 @@
 import Search from './models/Search';
 
-const search = new Search('pizza');
 
-search.getResults();
+/** Global state of the app
+ * Search object
+ * Current recipes object
+ * Shopping list object
+ * Liked recipes
+ */
+
+const state = {};
+
+const controlSearch = async () => {
+    //1.get query from view
+    const query = 'pizza';
+
+    if (query) {
+        //2. new search object add to state
+        state.search = new Search(query);
+
+        //3. prepare UI for results
+
+        //4. search for recipes
+        await state.search.getResults();
+
+        // 5. render results on UI 
+        console.log(state.search.result);
+    }
+
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+    console.log('hello');
+})
+
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -10,7 +10,6 @@ export default class Search {
         try {
             const res = await axios(`${config.crossOrigin}${config.url}?key=${config.key}&q=${this.query}`);
             this.result = res.data.recipes;
-            console.log(this.result);
         } catch (error) {
             console.log(error);
         }
